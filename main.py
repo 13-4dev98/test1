ee323775038e4d32d11980fe770b55e62771d72c
13-4dev98 <holandry52@gmail.com>
2024-11-20 12:10:12 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, Response, stream_with_context
 import requests
+import urllib.parse
 
 app = Flask(__name__)
 
@@ -11,8 +12,9 @@ def proxy_audio():
     if not url:
         return "URL не указан", 400
 
-    try:
+    encoded_filename = urllib.parse.quote(filename)
 
+    try:
         response = requests.get(url, stream=True)
         if response.status_code == 200:
 
@@ -20,7 +22,7 @@ def proxy_audio():
                 stream_with_context(response.iter_content(chunk_size=4096)),
                 headers={
                     "Content-Type": response.headers.get("Content-Type", "application/octet-stream"),
-                    "Content-Disposition": f'attachment; filename="{filename}"',
+                    "Content-Disposition": f'attachment; filename="{encoded_filename}"',
                 },
             )
         return "Не удалось скачать файл", response.status_code
